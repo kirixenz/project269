480720918bb11fba7e5d3a34c5a2ff998c206793
kirixenz <157266679+kirixenz@users.noreply.github.com>
2024-02-15 18:25:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
